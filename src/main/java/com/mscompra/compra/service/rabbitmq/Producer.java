c1eb4c5ccccb2b1b86cbc909187270cf0c110857
Carlos Augusto <augustino2003@hotmail.com>
2022-02-21 23:57:26 -0300

    Ajustes Producer e Consumer excluído

--- src/main/java/com/mscompra/compra/service/rabbitmq/Producer.java
+++ src/main/java/com/mscompra/compra/service/rabbitmq/Producer.java
@@ -1,28 +1,31 @@
 package com.mscompra.compra.service.rabbitmq;
 
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mscompra.compra.model.Pedido;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class Producer {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
 
-    @Autowired
-    private Queue queue;
+    private final RabbitTemplate rabbitTemplate;
 
+    private final Queue queue;
+
+    private final ObjectMapper mapper;
+
+    @SneakyThrows
     @PostMapping
     public void enviarPedido(Pedido pedido){
-        rabbitTemplate.convertAndSend(queue.getName(), pedido);
+        rabbitTemplate.convertAndSend(queue.getName(), mapper.writeValueAsString(pedido));
     }
 }
